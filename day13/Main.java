6b920b34def212b1d9159ecdc00444c8f6298195
Mete Can kaya <metecankaya.odtu@gmail.com>
2021-12-29 20:23:34 +0300

    day 13

--- day13/Main.java
+++ day13/Main.java
@@ -29,77 +29,63 @@ public class Main {
         return file_lines;
     }
 
-
     static void question_1() {
-        ArrayList<String> filelines = readFile("test_set.txt");
+        ArrayList<String> filelines = readFile("data.txt");
         ArrayList<ArrayList<Integer>> instructions = new ArrayList<ArrayList<Integer>>();
         Boolean first_parser = true;
         int highest_x = 0;
         int count = 0;
-        for (int i = 0; i < filelines.size(); i++)
-        {
-            if(filelines.get(i).equals(""))
-            {
+        int instruction_count = 0;
+        for (int i = 0; i < filelines.size(); i++) {
+            if (filelines.get(i).equals("")) {
                 first_parser = false;
                 continue;
             }
-            if (first_parser)
-            {
+            if (first_parser) {
                 String[] coordinates = filelines.get(i).split(",");
                 int x = Integer.parseInt(coordinates[0]);
                 int y = Integer.parseInt(coordinates[1]);
-                
-                if(y > instructions.size()-1)
-                {
-                    for(int k = instructions.size()-1; k <y; k++)
-                    {
+
+                if (y > instructions.size() - 1) {
+                    for (int k = instructions.size() - 1; k < y; k++) {
                         ArrayList<Integer> vector = new ArrayList<Integer>();
                         instructions.add(vector);
                     }
                 }
                 if (highest_x < x)
                     highest_x = x;
-                if(x > instructions.get(y).size() - 1)
-                {
-                    for(int k = instructions.get(y).size() - 1; k<x; k++)
-                    {
+                if (x > instructions.get(y).size() - 1) {
+                    for (int k = instructions.get(y).size() - 1; k < x; k++) {
                         instructions.get(y).add(0);
                     }
                 }
                 instructions.get(y).set(x, 1);
-                for(int j = 0; j<instructions.size(); j++)
-                {
-                    while(instructions.get(j).size()-1 < highest_x)
-                    {
+                for (int j = 0; j < instructions.size(); j++) {
+                    while (instructions.get(j).size() - 1 < highest_x) {
                         instructions.get(j).add(0);
                     }
                 }
-            }
-            else
-            {
+            } else {
+                if (instruction_count == 1)
+                    break;
+                instruction_count++;
                 String[] buffer = filelines.get(i).split(" ");
-                buffer = buffer[buffer.length - 1].split("="); 
+                buffer = buffer[buffer.length - 1].split("=");
                 String direction = buffer[0];
                 int amount = Integer.parseInt(buffer[1]);
-                if(direction.equals("y"))
-                {
+                if (direction.equals("y")) {
                     int reverse_index = amount;
-                    for(int j = amount+1; j<instructions.size(); j++)
-                    {
+                    for (int j = amount + 1; j < instructions.size(); j++) {
                         reverse_index -= 1;
-                        for(int k=0; k<instructions.get(j).size(); k++)
-                        {                            
-                            instructions.get(reverse_index).set(k, instructions.get(reverse_index).get(k) | instructions.get(j).get(k));
+                        for (int k = 0; k < instructions.get(j).size(); k++) {
+                            instructions.get(reverse_index).set(k,
+                                    instructions.get(reverse_index).get(k) | instructions.get(j).get(k));
                             instructions.get(j).set(k, 0);
                         }
                     }
-                }
-                else
-                {
-                    for(int j = 0; j<instructions.size(); j++)
-                    {
-                        for(int k=amount; k<instructions.get(j).size(); k++)
-                        {
+                } else {
+                    for (int j = 0; j < instructions.size(); j++) {
+                        for (int k = amount; k < instructions.get(j).size(); k++) {
                             int temp = instructions.get(j).get(k);
                             instructions.get(j).set(k, 0);
                             int reverse_index = amount - (k - amount);
@@ -108,26 +94,90 @@ public class Main {
                     }
                 }
             }
-            
+
         }
-        for (int j = 0; j<instructions.size(); j++)
-        {
-            for(int i=0; i<instructions.get(j).size(); i++)
-            {
-                if(instructions.get(j).get(i) == 0)
-                count +=1;
+        for (int j = 0; j < instructions.size(); j++) {
+            for (int i = 0; i < instructions.get(j).size(); i++) {
+                if (instructions.get(j).get(i) == 1)
+                    count += 1;
             }
         }
-        System.out.println(count); 
+        System.out.println(count);
     }
 
     static void question_2() {
         ArrayList<String> filelines = readFile("data.txt");
+        ArrayList<ArrayList<Integer>> instructions = new ArrayList<ArrayList<Integer>>();
+        Boolean first_parser = true;
+        int highest_x = 0;
+        int count = 0;
+        int instruction_count = 0;
+        for (int i = 0; i < filelines.size(); i++) {
+            if (filelines.get(i).equals("")) {
+                first_parser = false;
+                continue;
+            }
+            if (first_parser) {
+                String[] coordinates = filelines.get(i).split(",");
+                int x = Integer.parseInt(coordinates[0]);
+                int y = Integer.parseInt(coordinates[1]);
 
+                if (y > instructions.size() - 1) {
+                    for (int k = instructions.size() - 1; k < y; k++) {
+                        ArrayList<Integer> vector = new ArrayList<Integer>();
+                        instructions.add(vector);
+                    }
+                }
+                if (highest_x < x)
+                    highest_x = x;
+                if (x > instructions.get(y).size() - 1) {
+                    for (int k = instructions.get(y).size() - 1; k < x; k++) {
+                        instructions.get(y).add(0);
+                    }
+                }
+                instructions.get(y).set(x, 1);
+                for (int j = 0; j < instructions.size(); j++) {
+                    while (instructions.get(j).size() - 1 < highest_x) {
+                        instructions.get(j).add(0);
+                    }
+                }
+            } else {
+                instruction_count++;
+                System.out.println(filelines.get(i));
+                String[] buffer = filelines.get(i).split(" ");
+                buffer = buffer[buffer.length - 1].split("=");
+                String direction = buffer[0];
+                int amount = Integer.parseInt(buffer[1]);
+                if (direction.equals("y")) {
+                    int reverse_index = amount;
+                    for (int j = amount + 1; j < amount * 2; j++) {
+                        reverse_index -= 1;
+                        for (int k = 0; k < instructions.get(j).size(); k++) {
+                            instructions.get(reverse_index).set(k,
+                                    instructions.get(reverse_index).get(k) | instructions.get(j).get(k));
+                            instructions.get(j).set(k, 0);
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < instructions.size(); j++) {
+                        for (int k = amount; k < amount * 2; k++) {
+                            int temp = instructions.get(j).get(k);
+                            instructions.get(j).set(k, 0);
+                            int reverse_index = amount - (k - amount);
+                            instructions.get(j).set(reverse_index, instructions.get(j).get(reverse_index) | temp);
+                        }
+                    }
+                }
+            }
+
+        }
+        for (int j = 0; j < 6; j++) {
+            System.out.println(instructions.get(j).subList(0, 40).toString());
+        }
     }
 
     public static void main(String[] args) {
-        question_1();
-        //question_2();
+        // question_1();
+        question_2();
     }
 }
