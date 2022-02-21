52b98d7dc0f5c41bda07b98391723302a96b1b3f
metcan <metecankaya.odtu@gmail.com>
2022-02-21 12:34:06 +0300

    day23 copy paste

---
+++ day23/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    
+    public static void main(String[] args)
+    { 
+        Day23 solver = new Day23("data.txt");    
+        System.out.println(solver.solvePart1());
+        System.out.println(solver.solvePart2());
+        return;
+    }
+}
+
+
